18f55d7811d5633a6f9212193146f20fd9440e66
Yuya Isaka <zxwg6329@gmail.com>
2023-11-30 14:34:20 +0900

    minimal

--- examples/a04.rs
+++ examples/a04.rs
@@ -2,11 +2,7 @@ fn main() {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).unwrap();
     let n: u16 = s.trim().parse().unwrap();
-    let mut res = String::new();
     for i in (0..=9).rev() {
-        res.push(if (n >> i) & 1 == 1 { '1' } else { '0' });
+        print!("{}", (n >> i) & 1);
     }
-    println!("{res}");
 }
-
-// わかりやすいコードではないが、短くしたらこう書ける

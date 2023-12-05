194045c45f07fb4e752a1b25af0a36199254d17e
Yuya Isaka <zxwg6329@gmail.com>
2023-12-05 19:43:35 +0900

    fix

--- examples/bit_zenbunn.rs
+++ examples/bit_zenbunn.rs
@@ -1,28 +1,31 @@
 fn main() {
-    let nk = input_array();
-    let n = *nk.first().unwrap();
-    let k = *nk.get(1).unwrap();
-
-    let a = input_array();
-
-    for i in 1..(1 << n) {
-        let mut res: u64 = 0;
-        for (j, c) in (0..n).rev().enumerate() {
-            let tmp = i >> c & 1;
-            if tmp == 1 {
-                res += a[j];
-            }
-        }
-        if res == k {
-            println!("Yes");
-        }
-    }
-
-    println!("No");
-}
-
-fn input_array() -> Vec<u64> {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).unwrap();
-    s.split_whitespace().map(|s| s.parse().unwrap()).collect()
+    let nk: Vec<u32> = s
+        .split_whitespace()
+        .map(|tmp| tmp.parse().unwrap())
+        .collect();
+    let n = *nk.first().unwrap();
+    let k = *nk.get(1).unwrap();
+    s.clear();
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).unwrap();
+    let a: Vec<u32> = s
+        .split_whitespace()
+        .map(|tmp| tmp.parse().unwrap())
+        .collect();
+
+    for i in 0..(1 << n) {
+        let mut tmp = 0;
+        for j in 0..n {
+            if (i >> j & 1) == 1 {
+                tmp += a[j as usize];
+            }
+        }
+        if tmp == k {
+            println!("Yes");
+            return;
+        }
+    }
+    println!("No");
 }
